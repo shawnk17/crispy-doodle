d1c45fae8315e61942311a5506fe6dabf7a5ea49
shawn <axe2781@gmail.com>
2017-10-30 13:00:51 -0400

    tete

---
+++ js.js
@@ -0,0 +1,46 @@
+var firstName = document.getElementById("firstname"),
+    lastName = document.getElementById("last-name"),
+    salutation =document.getElementById("salutation"),
+    addButton = document.getElementById("add-button"),
+    addButton =document.getElementById("clear-button"),
+    displayDiv = document.getElementById("customer-list");
+
+var customer = function customer(firstName, lastName){
+    var myNewObj ={};
+
+};
+
+// var customer = function customer(firstname, lastName, salutation{
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.salutation = salutation;
+//     this.id = ++nexId;
+
+//     this.greetFormally = function greetFormally(){
+
+//     }
+
+// });
+    
+
+var renderCustomer = function renderCustomer(){
+    for(var i =0; i < customerList.length; i++){
+        var curCurstomerList[i];
+        displayDiv.innerHTML += i +":" + curCustomer,greetformally + "<br>"    }
+};
+
+var clearInputs = function clearInputs(){
+    firstName =" ";
+    lastName =" ";
+    salutation =" ";
+};
+
+// addButton.addEventListener("click", function {
+//     alert("Bang")
+// })
+
+clearInputs();
+renderCustomer=();
+
+clearButotn.
+
